0bb5e7528c1787d61c007c671f509b998bd57c37
Ronald D. Seron <89917742+Ronse11@users.noreply.github.com>
2025-03-05 12:41:09 +0800

    Update landing.blade.php

--- resources/views/navigations/landing.blade.php
+++ resources/views/navigations/landing.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student Academic Performance Tracker</title>
-    <link rel="icon" href="{{ Vite::asset('resources/images/saptlogo.svg')  }}">
+    <link rel="icon" href="{{ asset('images/saptlogo.svg') }}">
 
     @vite(['resources/css/app.css'])
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
