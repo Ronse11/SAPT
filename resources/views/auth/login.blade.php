8b6e84b73c983d70b0ba4b80d090a1ce627d5cab
Ronse11 <seronronalddalida@gmail.com>
2025-03-05 14:36:32 +0800

    git push origin main Change the location of the logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link rel="icon" href="{{ Vite::asset('resources/images/saptlogo.svg')  }}">
+    <link rel="icon" href="{{ asset('images/saptlogo.svg') }}">
     
     @vite(['resources/css/app.css', 'resources/css/breakPoints.css', 'resources/js/app.js', 'resources/js/auth.js'])
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
