4bca3034435db7674785c64cc6a55bb6746f3b78
Ronse11 <seronronalddalida@gmail.com>
2025-03-05 19:15:34 +0800

    changed the favicon

--- resources/views/teacher/calendar.blade.php
+++ resources/views/teacher/calendar.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Calendar</title>
-    <link rel="icon" href="{{ Vite::asset('resources/images/saptfavicon.svg')  }}">
+    <link rel="icon" href="{{ asset('images/saptlogo.svg') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link href='https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css' rel='stylesheet' />
